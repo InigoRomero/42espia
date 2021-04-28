d3dc2327fb3c5d1116ae3c20690fc3b89c1556e3
Iñigo Romero <45758515+InigoRomero@users.noreply.github.com>
2021-04-28 11:26:30 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 var oauth2 = require('simple-oauth2')({
-  clientID: '8f4c258b48e4a7d4b5928ee4c715bd3e74f6aab2a01e0811e8d24fcb5b70614f',
-  clientSecret: 'a32988c572880d41a074474b815589671ff1daa90aa8dd7618129b05fce0cba9',
+  clientID: '',
+  clientSecret: '',
   site: 'https://api.intra.42.fr',
   authorizationPath: 'https://api.intra.42.fr/oauth',
   tokenPath: 'https://api.intra.42.fr/oauth/token'
